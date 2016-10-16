045978c2ee55b78093222369ec68226ce2e0de88
likechen <chenlike6892753@126.com>
2016-10-16 13:48:27 +0800

    调整目录结构

--- src/main/java/com/github/likechen/web/blog/controller/HomeController.java
+++ src/main/java/com/github/likechen/web/blog/controller/HomeController.java
@@ -1,8 +1,19 @@
 package com.github.likechen.web.blog.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by like on 2016/10/15.
  */
+@RestController
+@RequestMapping(value = "", produces = "text/html;charset=UTF-8")
 public class HomeController {
 
+    @RequestMapping(value = "/*",  method = RequestMethod.GET)
+    public ModelAndView index(){
+        return new ModelAndView("index");
+    }
 }
---
+++ src/main/webapp/WEB-INF/fe/index.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>hello world</title>
+</head>
